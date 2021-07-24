fc508ccc130df9d43878f04e31d3780949c0005e
HASSAN <syed.hassan.ibrahim53@gmail.com>
2021-07-24 15:32:51 +0500

    (Fifth)Added new classes.

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+
+    private int carid;
+    private int car_driver_id;
+    private int car_customer_id;
+    private int car_number;
+    private String car_company;
+    private String car_type;
+    private String car_description;
+}
